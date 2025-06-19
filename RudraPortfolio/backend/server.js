886d5ac8af7f68aae27543b6804741d081568d27
Rudranarayan Nayak <161320704+rudranarayann@users.noreply.github.com>
2025-06-19 21:30:01 +0530

    Update server.js

--- RudraPortfolio/backend/server.js
+++ RudraPortfolio/backend/server.js
@@ -32,6 +32,6 @@ app.get('/',(req,res)=>{
 });
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(Server is running on http://localhost:${PORT});
     
-});
+});.this is my server .js
